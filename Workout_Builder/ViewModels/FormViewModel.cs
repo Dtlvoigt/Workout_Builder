82f7fa1dcfddea26182dbe68343b4c59b12bf78e
Dylan Voigt <dtlvoigt@gmail.com>
2024-01-20 17:23:49 -0800

    Added view models

---
+++ Workout_Builder/ViewModels/FormViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Workout_Builder.Models;
+
+namespace Workout_Builder.ViewModels
+{
+    public class FormViewModel
+    {
+        public Workout Workout { get; set; }
+        public Exercise Exercise { get; set; }
+        public ExerciseType ExerciseType { get; set; }
+        public Set Set { get; set; }
+    }
+}
---
+++ Workout_Builder/ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Workout_Builder.Models;
+
+namespace Workout_Builder.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Workout> WorkoutTemplates { get; set; }
+        public List<Workout> RecentWorkouts { get; set; }
+    }
+}
