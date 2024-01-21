75a950790b1b7f9df8e977c70cda0376e213dd42
Dylan Voigt <dtlvoigt@gmail.com>
2024-01-20 17:22:52 -0800

    Added interface class

---
+++ Workout_Builder/Services/IWorkoutService.cs
@@ -0,0 +1,18 @@
+﻿using Workout_Builder.Models;
+
+namespace Workout_Builder.Services
+{
+    public interface IWorkoutService
+    {
+        //search workouts
+        Task<List<Workout>> GetWorkoutTemplates(string userID);
+        Task<List<Exercise>> GetUserExercises(string userID);
+
+
+        //updating workouts
+
+        //non specific workout info
+
+        //user info
+    }
+}
