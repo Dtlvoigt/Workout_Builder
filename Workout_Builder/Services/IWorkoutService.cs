de79054674a422e3c32f514259565483c5cfeb61
Dylan Voigt <dtlvoigt@gmail.com>
2024-01-21 00:33:23 -0800

    Added new workout method

--- Workout_Builder/Services/IWorkoutService.cs
+++ Workout_Builder/Services/IWorkoutService.cs
@@ -5,11 +5,12 @@ namespace Workout_Builder.Services
     public interface IWorkoutService
     {
         //search workouts
-        Task<List<Workout>> GetWorkoutTemplates(string userID);
+        Task<List<Workout>> GetUserTemplates(string userID);
         Task<List<Exercise>> GetUserExercises(string userID);
 
 
         //updating workouts
+        System.Threading.Tasks.Task AddWorkout(Workout workout);
 
         //non specific workout info
 
--- Workout_Builder/Services/WorkoutService.cs
+++ Workout_Builder/Services/WorkoutService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
 using Workout_Builder.Data;
 using Workout_Builder.Models;
 
@@ -6,51 +7,49 @@ namespace Workout_Builder.Services
 {
     public class WorkoutService : IWorkoutService
     {
-        private IDbContextFactory<WorkoutContext> _dbContextFactory;
+        private IWorkoutContext _dbContext;
+        //private IDbContextFactory<WorkoutContext> _dbContext;
 
-        public WorkoutService(IDbContextFactory<WorkoutContext> dbContextFactory)
+        public WorkoutService(IWorkoutContext workoutContext/*IDbContextFactory<WorkoutContext> dbContextFactory*/)
         {
-            _dbContextFactory = dbContextFactory;
+            _dbContext = workoutContext;
         }
 
         public void AddExercise(Exercise exercise)
         {
-            using (var context = _dbContextFactory.CreateDbContext())
-            {
-                context.Exercises.Add(exercise);
-                context.SaveChanges();
-            }
+            //using (var context = _dbContext.CreateDbContext())
+            //{
+            //    context.Exercises.Add(exercise);
+            //    context.SaveChanges();
+            //}
         }
 
         public void UpdateExercise(Exercise exercise)
         {
-            if (exercise == null)
-            {
-                throw new Exception("Exercise " + exercise.Id + " not found.");
-            }
-            else
-            {
-                using (var context = _dbContextFactory.CreateDbContext())
-                {
-                    context.Update(exercise);
-                    context.SaveChanges();
-                }
-            }
+            //if (exercise == null)
+            //{
+            //    throw new Exception("Exercise " + exercise.Id + " not found.");
+            //}
+            //else
+            //{
+            //    using (var context = _dbContext.CreateDbContext())
+            //    {
+            //        context.Update(exercise);
+            //        context.SaveChanges();
+            //    }
+            //}
         }
 
         /////////////////////
         // search workouts //
         /////////////////////
 
-        public async Task<List<Workout>> GetWorkoutTemplates(string userID)
+        public async Task<List<Workout>> GetUserTemplates(string userID)
         {
-            using (var context = _dbContextFactory.CreateDbContext())
-            {
-                IQueryable<Workout> workoutQuery;
-                workoutQuery = context.Workouts.OrderBy(i => i.DateCreated)
-                                               .ThenBy(i => i.Name);
-                return await workoutQuery.ToListAsync().ConfigureAwait(false);
-            }            
+            IQueryable<Workout> workoutQuery;
+            workoutQuery = _dbContext.Workouts.OrderBy(i => i.DateCreated)
+                                            .ThenBy(i => i.Name);
+            return await workoutQuery.ToListAsync().ConfigureAwait(false);
         }
 
         public Task<List<Exercise>> GetUserExercises(string userID)
@@ -62,17 +61,32 @@ namespace Workout_Builder.Services
         // updating workouts //
         ///////////////////////
 
+        public async Task AddWorkout(Workout workout)
+        {
+            if (workout == null)
+            {
+                throw new Exception("workout is null");
+            }
+            if (String.IsNullOrWhiteSpace(workout.UserId))
+            {
+                throw new Exception("no userID");
+            }
+
+            workout.DateCreated = DateTime.Now;
+            workout.IsTemplate = true;
+
+            await _dbContext.Workouts.AddAsync(workout).ConfigureAwait(false);
+            await _dbContext.SaveChangesAsync(CancellationToken.None).ConfigureAwait(false);
+        }
 
         ///////////////////////////////
         // non specific workout info //
         ///////////////////////////////
         public ExerciseType GetExerciseByName(string name)
         {
-            using (var context = _dbContextFactory.CreateDbContext())
-            {
-                var exerciseType = context.ExerciseTypes.FirstOrDefault(x => x.Name == name);
-                return exerciseType ?? throw new Exception("Exercise " + name + " not found.");
-            }
+            var exerciseType = _dbContext.ExerciseTypes.FirstOrDefault(x => x.Name == name) 
+                ?? throw new Exception("Exercise " + name + " not found.");
+            return exerciseType;
         }
 
 
