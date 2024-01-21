0bf39ce33b539cfe87171c0bc4a026490aa55ce0
Dylan Voigt <dtlvoigt@gmail.com>
2024-01-21 00:30:05 -0800

    Changed viewmodel name

---
+++ Workout_Builder/ViewModels/newWorkoutViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Workout_Builder.Models;
+
+namespace Workout_Builder.ViewModels
+{
+    public class NewWorkoutVM
+    {
+        public Workout Workout { get; set; }
+        public List<Workout> CurrentTemplates { get; set; }
+        //public Exercise Exercise { get; set; }
+        //public ExerciseType ExerciseType { get; set; }
+        //public Set Set { get; set; }
+    }
+}
