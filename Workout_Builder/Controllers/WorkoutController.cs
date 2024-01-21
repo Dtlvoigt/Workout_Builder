f2536d9fc396d54fe6dfb83d0d2c71cbe72b53c0
Dylan Voigt <dtlvoigt@gmail.com>
2024-01-20 17:25:07 -0800

    Service class is not initialized in constructor and added logic to load users workout templates

--- Workout_Builder/Controllers/WorkoutController.cs
+++ Workout_Builder/Controllers/WorkoutController.cs
@@ -2,22 +2,53 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Security.Claims;
 using Workout_Builder.Models;
+using Workout_Builder.Services;
+using Workout_Builder.ViewModels;
 
 namespace Workout_Builder.Controllers
 {
     public class WorkoutController : Controller
     {
+        private readonly IWorkoutService _data;
         private readonly ILogger<WorkoutController> _logger;
 
-        public WorkoutController(ILogger<WorkoutController> logger)
+        public WorkoutController(IWorkoutService data, ILogger<WorkoutController> logger)
         {
+            _data = data;
             _logger = logger;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            //var userID = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            return View();
+            var userID = User.FindFirstValue(ClaimTypes.NameIdentifier) ?? throw new Exception("User is null");
+            var templates = await _data.GetWorkoutTemplates(userID).ConfigureAwait(false);
+            var homeVM = new HomeViewModel()
+            {
+                WorkoutTemplates = templates,
+            };
+            return View(homeVM);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> NewWorkout()
+        {
+            var newWorkout = new Workout();
+            newWorkout.IsTemplate = true;
+            var createVM = new FormViewModel()
+            {
+                Workout = newWorkout
+            };
+
+            return View(createVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> NewWorkout(FormViewModel createVM)
+        {
+            //add workout to database
+
+            return RedirectToAction("Index");
         }
 
         public IActionResult Privacy()
