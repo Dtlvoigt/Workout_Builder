012ab76e94e86a76408624f0e6973eac77434a2f
Dylan Voigt <dtlvoigt@gmail.com>
2024-02-04 22:36:33 -0800

    Added a lastUpdated field to exercise types

---
+++ Workout_Builder/Migrations/20240205061852_added lastUpdated field to exercise types.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Workout_Builder.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedlastUpdatedfieldtoexercisetypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdated",
+                table: "ExerciseTypes",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastUpdated",
+                table: "ExerciseTypes");
+        }
+    }
+}
--- Workout_Builder/Models/ExerciseType.cs
+++ Workout_Builder/Models/ExerciseType.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace Workout_Builder.Models
 {
@@ -7,15 +8,25 @@ namespace Workout_Builder.Models
     {
         [Key]
         public int Id { get; set; }
+        [JsonPropertyName("name")]
         public string? Name { get; set; }
         public string? Description { get; set; }
+        [JsonPropertyName("force")]
         public string? Force {  get; set; }
+        [JsonPropertyName("level")]
         public string? Level { get; set; }
+        [JsonPropertyName("mechanic")]
         public string? Mechanic { get; set; }
+        [JsonPropertyName("equipment")]
         public string? Equipment { get; set; }
+        [JsonPropertyName("primaryMuscles")]
         public string[]? PrimaryMuscles { get; set; }
+        [JsonPropertyName("secondaryMuscles")]
         public string[]? SecondaryMuscles { get; set; }
+        [JsonPropertyName("instructions")]
         public string[]? Instructions {  get; set; }
+        [JsonPropertyName("category")]
         public string? Category { get; set; }
+        public DateTime? LastUpdated { get; set; }
     }
 }
