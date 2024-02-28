c47eb6b4fd72b2289b8d194eb6131770db428665
Dylan Voigt <dtlvoigt@gmail.com>
2024-02-27 22:36:34 -0800

    Changed this model to only hold relevant info

--- Workout_Builder/Models/Set.cs
+++ Workout_Builder/Models/Set.cs
@@ -5,13 +5,16 @@ namespace Workout_Builder.Models
 {
     public class Set
     {
-        [Key]
-        public int Id { get; set; }
-        public Exercise? Exercise {  get; set; }
-        public int Order { get; set; }
-        public Boolean ForTime { get; set; }
-        public int Duration { get; set; }
+        //[Key]
+        //public int Id { get; set; }
+        //public Exercise? Exercise {  get; set; }
+        //public int Order { get; set; }
+        //public Boolean ForTime { get; set; }
+        //public int Duration { get; set; }
+        //public int Reps { get; set; }
+        //public Boolean Completed { get; set; } = false;
+
         public int Reps { get; set; }
-        public Boolean Completed { get; set; } = false;
+        public int Weight { get; set; }
     }
 }
