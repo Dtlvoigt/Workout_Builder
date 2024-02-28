322595c2c620e647bb654fb0d7854772f923bfb1
Dylan Voigt <dtlvoigt@gmail.com>
2024-02-27 22:37:43 -0800

    Added a list of Set objects to the model

--- Workout_Builder/ViewModels/NewExerciseViewModel.cs
+++ Workout_Builder/ViewModels/NewExerciseViewModel.cs
@@ -7,7 +7,13 @@ namespace Workout_Builder.ViewModels
     {
         public Exercise Exercise { get; set; }
         public List<SelectListItem> ExerciseList { get; set; }
-        public String? SelectedExercise {  get; set; }
+        public String? SelectedExercise { get; set; }
         public Boolean Deleted { get; set; }
+        public int MaxNumExercises { get; set; }
+        public int Order { get; set; }
+        public int NumSets { get; set; }
+        public int MasterReps { get; set; }
+        public int MasterWeight { get; set; }
+        public List<Set> SetsList { get; set; }
     }
 }
