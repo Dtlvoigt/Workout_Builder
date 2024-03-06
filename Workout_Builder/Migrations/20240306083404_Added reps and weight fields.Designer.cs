1eb65df0664828351e7ae1802ca90afa4f86c3b5
Dylan Voigt <dtlvoigt@gmail.com>
2024-03-06 00:34:29 -0800

    Added reps and weight fields to entity

--- Workout_Builder/Migrations/20240306083404_Added reps and weight fields.Designer.cs
+++ Workout_Builder/Migrations/20240306083404_Added reps and weight fields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Workout_Builder.Data;
 
@@ -11,9 +12,11 @@ using Workout_Builder.Data;
 namespace Workout_Builder.Migrations
 {
     [DbContext(typeof(WorkoutContext))]
-    partial class WorkoutContextModelSnapshot : ModelSnapshot
+    [Migration("20240306083404_Added reps and weight fields")]
+    partial class Addedrepsandweightfields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Workout_Builder/Migrations/20240306083404_Added reps and weight fields.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Workout_Builder.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addedrepsandweightfields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CustomExercise",
+                table: "Exercises",
+                newName: "CustomExerciseName");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CustomSets",
+                table: "Exercises",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MasterReps",
+                table: "Exercises",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MasterWeight",
+                table: "Exercises",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomSets",
+                table: "Exercises");
+
+            migrationBuilder.DropColumn(
+                name: "MasterReps",
+                table: "Exercises");
+
+            migrationBuilder.DropColumn(
+                name: "MasterWeight",
+                table: "Exercises");
+
+            migrationBuilder.RenameColumn(
+                name: "CustomExerciseName",
+                table: "Exercises",
+                newName: "CustomExercise");
+        }
+    }
+}
