5ae2d393e5dda36f015f442a25c118c0145311ee
Dylan Voigt <dtlvoigt@gmail.com>
2024-03-07 23:45:56 -0800

    Added controller action for loading edit page

--- Workout_Builder/Controllers/WorkoutController.cs
+++ Workout_Builder/Controllers/WorkoutController.cs
@@ -93,49 +93,6 @@ namespace Workout_Builder.Controllers
                 throw new Exception("View Model is missing data");
             }
 
-            //remove unused exercises
-            //newWorkoutVM.ExerciseModels.RemoveAll(e => e.Order >= newWorkoutVM.NumExercises);
-            //for (int i = newWorkoutVM.MaxNumExercises; i > newWorkoutVM.NumExercises; i--)
-            //{
-                
-            //    newWorkoutVM.ExerciseModels.RemoveAt(i);
-            //}
-            //newWorkoutVM.Exercises.RemoveAll(e => e.NumSets == 0);
-
-            //if the user added a new exercise, return the viewmodel
-            //if (newWorkoutVM.AddExercise == true)
-            //{
-            //    //foreach (var exercise in  newWorkoutVM.Exercises)
-            //    //{
-            //    //    if (exercise.NumSets == 0)
-            //    //    {
-            //    //        newWorkoutVM.Exercises.Remove(exercise);
-            //    //    }
-            //    //}
-
-            //    //add new exercise
-            //    //newWorkoutVM.Exercise.Add(new Exercise()
-            //    //{
-            //    //    NumSets = 1,
-            //    //});
-            //    //newWorkoutVM.ExerciseModels.ForEach(async e => { e.ExerciseList = await _workoutContext.GetExerciseSelectList(); });
-            //    foreach(var exerciseModel in newWorkoutVM.ExerciseModels)
-            //    {
-            //        exerciseModel.ExerciseList = await _workoutContext.GetExerciseSelectList().ConfigureAwait(false);
-            //    }
-
-            //    newWorkoutVM.ExerciseModels.Add(new NewExerciseViewModel()
-            //    {
-            //        Deleted = false,
-            //        Exercise = new Exercise()
-            //        {
-            //            NumSets = 1
-            //        },
-            //        ExerciseList = await _workoutContext.GetExerciseSelectList(),
-            //    });
-
-            //    return View(newWorkoutVM);
-            //}
             if (ModelState.IsValid)
             {
                 if (newWorkoutVM.Workout == null)
@@ -143,31 +100,17 @@ namespace Workout_Builder.Controllers
                     throw new Exception("Workout is null");
                 }
 
-                //check if any exercise names are empty
-                for(int i = 0; i <= newWorkoutVM.NumExercises; i++)
-                {
-                    //if (String.IsNullOrEmpty(newWorkoutVM.ExerciseModels[i].Exercise.ExerciseType.Name))
-                    //{
-                    //        ModelState.AddModelError(nameof(newWorkoutVM.ExerciseModels[i].Exercise.ExerciseType.Name), "Exercise name required");
-                    //}
-
-                }
-
-                //remove deleted exercises
-                //newWorkoutVM.ExerciseModels.RemoveAll(e => e.Deleted == true);
-
                 //create workout for current user
                 var userID = User.FindFirstValue(ClaimTypes.NameIdentifier) ?? throw new Exception("UserId is null");
                 newWorkoutVM.Workout.UserId = userID;
 
                 //add workout to database
                 int workoutID = await _workoutContext.AddWorkout(newWorkoutVM.Workout).ConfigureAwait(false);
-                //newWorkoutVM.ExerciseModels.ForEach(e => { e.Exercise.Workout.Id = workoutID; });
 
                 //add exercises to database
                 for (int i = 0; i < newWorkoutVM.NumExercises; i++)
                 {
-                    //obtain necessary information
+                    //obtain exercise information
                     var model = newWorkoutVM.ExerciseModels[i];
                     var exerciseType = await _workoutContext.GetExerciseByName(model.Name);
 
@@ -225,9 +168,42 @@ namespace Workout_Builder.Controllers
         }
 
         [HttpGet]
+        [Route("Workout/EditWorkout/{workoutID:int}")]
         public async Task<IActionResult> EditWorkout(int workoutID)
         {
 
+            var newWorkoutVM = new WorkoutVM()
+            {
+                Workout = new Workout(),
+                ExerciseModels = new List<ExerciseVM>(),
+                NumExercises = 1,
+                MaxNumExercises = _maxExercises,
+                MaxNumSets = _maxSets,
+            };
+
+            var exerciseList = await _workoutContext.GetExerciseSelectList();
+            for (int i = 0; i < _maxExercises; i++)
+            {
+                var newModel = new ExerciseVM()
+                {
+                    Name = "",
+                    Order = i,
+                    NumSets = 1,
+                    ExerciseList = exerciseList,
+                    MaxNumExercises = _maxExercises,
+                    SetsList = new List<Set>(),
+                    CustomSets = false,
+                };
+
+                for (int j = 0; j < _maxSets; j++)
+                {
+                    newModel.SetsList.Add(new Set());
+                }
+
+                newWorkoutVM.ExerciseModels.Add(newModel);
+            }
+
+            return View(newWorkoutVM);
         }
 
         public IActionResult NewExercisePartial(int orderNum)
