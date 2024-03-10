e576facf653d0cc1cc6c04b2dd6c55859314b990
Dylan Voigt <dtlvoigt@gmail.com>
2024-03-10 01:03:03 -0800

    Cleaned up some unused fields and added constructors for a new exercise and existing one

--- Workout_Builder/ViewModels/ExerciseViewModel.cs
+++ Workout_Builder/ViewModels/ExerciseViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Configuration;
 using System.ComponentModel.DataAnnotations;
 using Workout_Builder.Models;
 
@@ -6,13 +7,42 @@ namespace Workout_Builder.ViewModels
 {
     public class ExerciseVM
     {
+        public ExerciseVM(int order)
+        {
+            Name = "";
+            Order = order;
+            NumSets = 1;
+            SetsList = new List<Set>();
+            CustomSets = false;
+        }
+
+        public ExerciseVM(Exercise exercise)
+        {
+            if(String.IsNullOrEmpty(exercise.CustomExerciseName))
+            {
+                if (exercise.ExerciseType == null)
+                {
+                    throw new Exception("Exercise type is null on exercise: " + exercise.Id);
+                }
+                Name = exercise.ExerciseType.Name;
+            }
+            else
+            {
+                Name = exercise.CustomExerciseName;
+            }
+            
+            Order = exercise.Order;
+            NumSets = exercise.NumSets;
+            MasterReps = exercise.MasterReps;
+            MasterWeight = exercise.MasterWeight;
+            SetsList = new List<Set>();
+            CustomSets = exercise.CustomSets;
+        }
+
         //public Exercise Exercise { get; set; }
         [Required(ErrorMessage = "Exercise name is required")]
         public string Name { get; set; }
         public List<SelectListItem>? ExerciseList { get; set; }
-        //public String? SelectedExercise { get; set; }
-        //public Boolean Deleted { get; set; }
-        public int MaxNumExercises { get; set; }
         public int Order { get; set; }
         public int NumSets { get; set; }
         public int MasterReps { get; set; }
