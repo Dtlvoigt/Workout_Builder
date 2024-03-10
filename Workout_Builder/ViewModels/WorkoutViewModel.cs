e89aac416b89b629998c5d8455a7299ff82456fb
Dylan Voigt <dtlvoigt@gmail.com>
2024-03-10 01:04:13 -0800

    Cleaned up some of the unused fields and commented out code

--- Workout_Builder/ViewModels/WorkoutViewModel.cs
+++ Workout_Builder/ViewModels/WorkoutViewModel.cs
@@ -4,19 +4,12 @@ namespace Workout_Builder.ViewModels
 {
     public class WorkoutVM
     {
-        public Workout? Workout { get; set; }
+        public Workout Workout { get; set; }
         public List<Workout> CurrentTemplates { get; set; }
         public List<ExerciseVM> ExerciseModels { get; set; }
-        //public List<Exercise> Exercises { get; set; }
-        //public Exercise NewExercise { get; set; }
-        //public Boolean AddExercise { get; set; }
         public int MaxNumExercises { get; set; }
         public int MaxNumSets { get; set; }
         public int NumExercises { get; set; }
         public Boolean IsPounds { get; set; } = true;
-        //public List<Set> Sets { get; set; }
-        //public Exercise Exercise { get; set; }
-        //public ExerciseType ExerciseType { get; set; }
-        //public Set Set { get; set; }
     }
 }
